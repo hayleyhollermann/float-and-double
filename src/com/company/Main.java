e937adc561b26f21386f35764ee36181a51eb408
Hayley Hollermann <hayleyhollermann@gmail.com>
2020-02-26 11:59:06 -0600

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // FLOAT
+        float myMinFloatValue = Float.MIN_VALUE;
+        float myMaxFloatValue = Float.MAX_VALUE;
+        System.out.println("Float min value = " + myMinFloatValue);
+        System.out.println("Float max value = " + myMaxFloatValue);
+        
+        // DOUBLE
+        double myMinDoubleValue = Double.MIN_VALUE;
+        double myMaxDoubleValue = Double.MAX_VALUE;
+        System.out.println("Double min value = " + myMinDoubleValue);
+        System.out.println("Double max value = " + myMaxDoubleValue);
+
+    }
+}
